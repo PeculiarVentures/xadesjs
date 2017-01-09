8a90b4ebe23c080df718548f2a2a903ee645e092
microshine <microshine@mail.ru>
2017-01-09 23:19:57 +0300

    Support signing options - signerRole - productionPlace - signing certificate

--- src/signed_xml.ts
+++ src/signed_xml.ts
@@ -1,11 +1,39 @@
 import * as XmlCore from "xml-core";
 import * as XmlDSigJs from "xmldsigjs";
+
 import * as XAdES from "./xml";
 
 const XADES_REFERENCE_TYPE = "http://uri.etsi.org/01903#SignedProperties";
 
+export interface OptionsSignerRole {
+    claimed?: string[];
+    certified?: string[];
+}
+
+export interface OptionsProductionPlace {
+    city?: string;
+    state?: string;
+    code?: string;
+    country?: string;
+}
+
+export interface OptionsPolicyIdentifier {
+
+}
+
 export interface OptionsXAdES extends XmlDSigJs.OptionsSign {
 
+    /**
+     * Sets a certificate of signer for signature. Optional
+     * 
+     * @type {string} base64 string of X509 certificate
+     * @memberOf OptionsXAdES
+     */
+    signingCertificate?: string;
+
+    policy?: OptionsPolicyIdentifier;
+    productionPlace?: OptionsProductionPlace;
+    signerRole?: OptionsSignerRole;
 }
 
 export class SignedXml extends XmlDSigJs.SignedXml {
@@ -71,27 +99,139 @@ export class SignedXml extends XmlDSigJs.SignedXml {
         this.XmlSignature.ObjectList.Add(dataObject);
     }
 
-    protected ApplySignOptions(signature: XmlDSigJs.Signature, algorithm: Algorithm, key: CryptoKey, options: OptionsXAdES): PromiseLike<void> {
-        return Promise.resolve()
-            .then(() => super.ApplySignOptions(signature, algorithm, key, options))
-            .then(() => {
-                if (this.Properties) {
-                    // Add SigningTime
-                    let sigProps = this.Properties.SignedProperties.SignedSignatureProperties;
-                    sigProps.SigningTime = new Date();
+    protected async ApplySignOptions(signature: XmlDSigJs.Signature, algorithm: Algorithm, key: CryptoKey, options: OptionsXAdES) {
+        await super.ApplySignOptions(signature, algorithm, key, options);
+        if (this.Properties) {
+            // Add SigningTime
+            let sigProps = this.Properties.SignedProperties.SignedSignatureProperties;
+            sigProps.SigningTime = new Date();
 
-                    // Add reference for SignedProperties
-                    let signingAlg = XmlCore.assign({}, algorithm, key.algorithm);
-                    let xadesRefHash = signingAlg.hash;
-                    let xadesRef = new XmlDSigJs.Reference();
-                    xadesRef.Type = XADES_REFERENCE_TYPE;
-                    xadesRef.Uri = `#${this.Properties.SignedProperties.Id}`;
-                    xadesRef.DigestMethod.Algorithm = XmlDSigJs.CryptoConfig.GetHashAlgorithm(xadesRefHash).namespaceURI;
+            // Add reference for SignedProperties
+            let signingAlg = XmlCore.assign({}, algorithm, key.algorithm);
+            let xadesRefHash = signingAlg.hash;
+            let xadesRef = new XmlDSigJs.Reference();
+            xadesRef.Type = XADES_REFERENCE_TYPE;
+            xadesRef.Uri = `#${this.Properties.SignedProperties.Id}`;
+            xadesRef.DigestMethod.Algorithm = XmlDSigJs.CryptoConfig.GetHashAlgorithm(xadesRefHash).namespaceURI;
 
-                    signature.SignedInfo.References.Add(xadesRef);
+            signature.SignedInfo.References.Add(xadesRef);
+
+            await this.ApplySigningCertificate(options.signingCertificate);
+            this.ApplySignatureProductionPlace(options.productionPlace);
+            this.ApplySignerRoles(options.signerRole);
+        }
+    }
+
+    Sign(algorithm: Algorithm, key: CryptoKey, data: Document, options?: OptionsXAdES): PromiseLike<XmlDSigJs.Signature> {
+        return super.Sign.apply(this, arguments);
+    }
+
+    protected async ApplySigningCertificate(base64string?: string) {
+        if (this.Properties && base64string) {
+            let raw = XmlCore.Convert.FromBase64(base64string);
+            let cert = new XmlDSigJs.X509Certificate(raw);
+
+            let ssp = this.Properties.SignedProperties.SignedSignatureProperties;
+            if (ssp.SigningCertificate.Count) {
+                throw new XmlCore.XmlError(XmlCore.XE.XML_EXCEPTION, "Signature can contain only one SigningCertificate");
+            }
+            let signingCertificate = new XAdES.Cert();
+            signingCertificate.IssuerSerial.X509IssuerName = cert.Issuer;
+            signingCertificate.IssuerSerial.X509SerialNumber = cert.SerialNumber; // TODO: Must be Big number here
+
+            let alg = XmlDSigJs.CryptoConfig.GetHashAlgorithm("SHA-256");
+            signingCertificate.CertDigest.DigestMethod.Algorithm = alg.namespaceURI;
+            signingCertificate.CertDigest.DigestValue = new Uint8Array(await cert.Thumbprint(alg.algorithm.name as any));
+
+            this.Properties.SignedProperties.SignedSignatureProperties.SigningCertificate.Add(signingCertificate);
+        }
+    }
+
+    protected ApplySignaturePolicyIdentifier() {
+        if (this.Properties) {
+            // let ssp = this.Properties.SignedProperties.SignedSignatureProperties;
+        }
+    }
+
+    protected ApplySignatureProductionPlace(options?: OptionsProductionPlace) {
+        if (this.Properties && options) {
+            let ssp = this.Properties.SignedProperties.SignedSignatureProperties;
+
+            if (options.city)
+                ssp.SignatureProductionPlace.City = options.city;
+
+            if (options.code)
+                ssp.SignatureProductionPlace.PostalCode = options.code;
+
+            if (options.country)
+                ssp.SignatureProductionPlace.CountryName = options.country;
+
+            if (options.state)
+                ssp.SignatureProductionPlace.StateOrProvince = options.state;
+        }
+    }
+
+    protected ApplySignerRoles(options?: OptionsSignerRole) {
+        if (this.Properties && options) {
+            let ssp = this.Properties.SignedProperties.SignedSignatureProperties;
+
+            if (options.claimed)
+                options.claimed.forEach(role => {
+                    let claimedRole = new XAdES.ClaimedRole();
+                    claimedRole.Value = role;
+                    ssp.SignerRole.ClaimedRoles.Add(claimedRole);
+                });
+
+            if (options.certified)
+                options.certified.forEach(role => {
+                    let certifiedRole = new XAdES.CertifiedRole();
+                    certifiedRole.Encoding = "der";
+                    certifiedRole.Value = XmlCore.Convert.FromBase64(role);
+                    ssp.SignerRole.CertifiedRoles.Add(certifiedRole);
+                });
+
+        }
+    }
+
+    protected async VerifySigningCertificate() {
+        let x509: XmlDSigJs.X509Certificate | null = null;
+        if (this.XmlSignature && this.Properties) {
+
+            let ssp = this.Properties.SignedProperties.SignedSignatureProperties;
+            if (ssp.SigningCertificate.Count !== 1) {
+                throw new XmlCore.XmlError(XmlCore.XE.XML_EXCEPTION, "Signature has got wrong amount of SigningCertificate, MUST be one");
+            }
+
+            // find certificate by Thumbprint
+            let alg = XmlDSigJs.CryptoConfig.GetHashAlgorithm("SHA-256");
+            let signingCertificate = ssp.SigningCertificate.Item(0) !;
+            const b64CertDigest = XmlCore.Convert.ToBase64(signingCertificate.CertDigest.DigestValue);
+            let keyInfos = this.XmlSignature.KeyInfo;
+            for (let i = 0; i < keyInfos.Count, !x509; i++) {
+                let item = keyInfos.Item(i);
+                if (item instanceof XmlDSigJs.KeyInfoX509Data) {
+                    let certs = item.Certificates;
+                    for (let j = 0; j < certs.length, !x509; j++) {
+                        let cert = certs[j];
+                        if (!cert)
+                            continue;
+                        const hash = new Uint8Array(await cert.Thumbprint(alg.algorithm as any));
+                        const b64Hash = XmlCore.Convert.ToBase64(hash);
+                        if (b64Hash === b64CertDigest)
+                            x509 = cert;
+                    }
                 }
+            }
+            if (!(
+                x509 &&
+                x509.Issuer === signingCertificate.IssuerSerial.X509IssuerName &&
+                x509.SerialNumber === signingCertificate.IssuerSerial.X509SerialNumber
+            ))
+                throw new XmlCore.XmlError(XmlCore.XE.XML_EXCEPTION, "SigningCertificate not found");
 
-            });
+        }
+
+        return x509;
     }
 
 }
