8db6cd83e716f002bbd24693d72d25bb7842b86f
microshine <microshine@mail.ru>
2018-02-09 18:13:27 +0300

    Fix rollup config

--- rollup.config.web.js
+++ rollup.config.web.js
@@ -3,20 +3,20 @@ import typescript from "rollup-plugin-typescript";
 
 let pkg = require("./package.json");
 
-let sourceMap = process.argv.some(item => item.toLowerCase() === "--dev");
+let sourcemap = process.argv.some(item => item.toLowerCase() === "--dev");
 
 export default {
-    entry: "src/index.ts",
+    input: "src/index.ts",
+    output: [
+        {
+            file: "dist/xades.js",
+            format: "umd",
+            sourcemap
+        }
+    ],
+    name: "XAdES",
     plugins: [
         typescript({ typescript: require("typescript"), target: "es5" }),
         nodeResolve({ jsnext: true, main: true }),
     ],
-    targets: [
-        {
-            dest: "dist/xades.js",
-            format: "umd",
-            moduleName: "XAdES",
-            sourceMap
-        }
-    ]
 };
