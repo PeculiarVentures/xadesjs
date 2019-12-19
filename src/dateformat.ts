ecf0250b4eb4770d9c8d908a8014c5fc8d4966ee
microshine <microshine@mail.ru>
2019-12-19 20:06:20 +0300

    Fix lint errors

--- src/dateformat.ts
+++ src/dateformat.ts
@@ -118,14 +118,14 @@ const masks: { [key: string]: string } = {
 const i18n = {
     dayNames: [
         "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",
-        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
     ],
     monthNames: [
         "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
-        "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+        "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",
     ],
     timeNames: [
-        "a", "p", "am", "pm", "A", "P", "AM", "PM"
+        "a", "p", "am", "pm", "A", "P", "AM", "PM",
     ],
 };
 
