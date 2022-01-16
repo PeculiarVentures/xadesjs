0bf70e3f580dd65c6ae5250cbbfc307e02e112bc
eidins <92649203+eidins@users.noreply.github.com>
2022-01-16 15:53:53 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as xadesXml from "./xml";
 
 export let xml = xadesXml;
-export * from "./signed_xml";
+export default * from "./signed_xml";
 
 export { Application } from "xmldsigjs";
 export { Select, Parse, Convert } from "xml-core";
